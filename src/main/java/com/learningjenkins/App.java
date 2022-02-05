76621ee5af1c18903c0d29d3f5d6120c23b4af67
Ahmed Araby <ahmedaraby605@gmail.com>
2022-02-05 09:57:52 +0200

    Update App.java

--- src/main/java/com/learningjenkins/App.java
+++ src/main/java/com/learningjenkins/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String hostServer = System.getenv("HOST_SERVER");
+        String buildId = System.getenv("BUILD_ID");
+        System.out.println("(custom defiend env v)host server = " + hostServer);
+        System.out.println("(predefined env v)build Id = " + buildId);
+        return ; 
     }
 }
